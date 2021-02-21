fef47a65f86fa6c41912fe11de528ae676300af4
lantanakamara <asadatnish@gmail.com>
2021-02-21 18:52:31 -0500

    converted the server to multi-client model that will be able to accept multiple clients at once

--- example.js
+++ example.js
@@ -1,88 +1,97 @@
-var net = require("net");
+const net = require("net");
 const { createLogger, format, transports } = require("winston");
-const serverAddress = "config.env";
+const dotenv = require("dotenv");
 
+dotenv.config();
 const { combine, timestamp, label, printf } = format;
+const server = net.createServer();
 
-var server = net.createServer(function (socket) {
-  socket.write("Hello From Server...\r\n");
-  //socket.pipe(socket);
+server.listen(process.env.PORT, process.env.IP_ADDRESS, function () {
+  console.log(`sever listening at...${process.env.PORT}`);
+});
+
+let clientsList = [];
+
+server.on("connection", function (socket) {
+  let newClient = socket.remoteAddress;
+  socket.write("Hollaaaaa");
+  clientsList.push(newClient);
 
   const myFormat = printf(({ level, message, IP, label, timestamp }) => {
-    return `Time_Stamp: ${timestamp} | label: ${label} | level:  ${level} | Message: ${message} | IP Address: ${IP}`;
+    return `Time_Stamp: ${timestamp} | level: ${level}  | label:  ${label} | Message: ${message} | IP Address: ${IP}`;
+  });
+
+  const logger = createLogger({
+    //level: "info",
+    format: combine(
+      format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+      myFormat
+    ),
+    defaultMeta: { service: "user-service" },
+    transports: [new transports.File({ filename: "combined.txt" })],
+  });
+
+  logger.log({
+    level: "info",
+    label: "New Client Connected!",
+    message: "Connected",
+    IP: server.address().address,
   });
 
   socket.on("data", function (data) {
-    const logger = createLogger({
-      level: "info",
-      format: combine(
-        format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-
-        //timestamp(),
-        myFormat
-      ),
-      defaultMeta: { service: "user-service" },
-      transports: [new transports.File({ filename: "combined.txt" })],
-    });
-    //const obj = JSON.parse(data);
-    console.log("DATA " + socket.remoteAddress);
-    console.log("client says: " + data);
-
-    logger.log({
-      level: "info",
-      label: "Client Connected!",
-      message: data.toString(),
-      IP: server.address().address,
+    console.log("DATA " + socket.remoteAddress + ": " + data);
+    // Write the data back to all the connected, the client will receive it as data from the server
+    clientsList.forEach(function (sock, index, array) {
+      socket.write(
+        socket.remoteAddress + ":" + socket.remotePort + " said " + data + "\n"
+      );
+      console.log(
+        socket.remoteAddress +
+          ":" +
+          socket.remotePort +
+          " said " +
+          data.toString() +
+          "\n"
+      );
     });
   });
 
   socket.on("error", function (error) {
-    const logger = createLogger({
-      level: "info",
-      format: combine(
-        format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-
-        //timestamp(),
-        myFormat
-      ),
-      defaultMeta: { service: "user-service" },
-      transports: [
-        new transports.File({ filename: "combined.txt", level: "error" }),
-      ],
-    });
-
     logger.log({
       level: "error",
       label: "Error occurred!",
-      message: error,
-      IP: socket.remoteAddress,
+      message: error.message,
+      IP: server.address().address,
     });
-    console.log("Error : " + error);
+    console.log("Error : " + error.stack);
   });
 
   socket.on("close", function (data) {
-    const logger = createLogger({
-      level: "info",
-      format: combine(
-        format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-
-        //timestamp(),
-        myFormat
-      ),
-      defaultMeta: { service: "user-service" },
-      transports: [new transports.File({ filename: "combined.txt" })],
+    let index = clientsList.findIndex((client) => {
+      return client.remoteAddress === socket.remoteAddress;
     });
 
+    if (index !== -1) {
+      clientsList.splice(index, 1);
+    }
+
+    clientsList.forEach((client) => {
+      socket.write(`${client.remoteAddress}: disconnected`);
+      console.log(`connection closed: ${client.remoteAddress}`);
+
+      logger.log({
+        level: "warn",
+        label: "Client disconnected!",
+        message: data,
+        IP: socket.remoteAddress,
+      });
+    });
     logger.log({
-      level: "info",
+      level: "warn",
       label: "Client disconnected!",
       message: data,
-      IP: socket.remoteAddress,
+      IP: server.address().address,
     });
-    console.log("connection closed");
+    console.log("connection closed \n\n");
   });
-});
-
-server.listen(serverAddress.labelSERVER_PORT, "127.0.0.1", function () {
-  console.log(`sever listening at...${process.env.PORT}`);
 });
