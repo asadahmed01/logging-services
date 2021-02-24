11bb6a35657cb1d7a59626a7fd53029d55212e2e
lantanakamara <asadatnish@gmail.com>
2021-02-24 05:30:14 -0500

    wrote the code that will log the automatic test logs and the manual test logs

--- example.js
+++ example.js
@@ -2,96 +2,178 @@ const net = require("net");
 const { createLogger, format, transports } = require("winston");
 const dotenv = require("dotenv");
 
+let clientsList = [];
 dotenv.config();
 const { combine, timestamp, label, printf } = format;
+
+//create the format of the log files
+const myFormat = printf(({ level, message, IP, label, timestamp }) => {
+  return `Time_Stamp: ${timestamp} | level: ${level.toUpperCase()}  | label:  ${label} | Message: ${message} | IP Address: ${IP}`;
+});
+
+//create the logger class
+
+const logger = createLogger({
+  //level: "info",
+  format: combine(
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    myFormat
+  ),
+  defaultMeta: { service: "user-service" },
+  transports: [new transports.File({ filename: "logs.txt" })],
+});
+
 const server = net.createServer();
 
 server.listen(process.env.PORT, process.env.IP_ADDRESS, function () {
   console.log(`sever listening at...${process.env.PORT}`);
 });
 
-let clientsList = [];
-
 server.on("connection", function (socket) {
-  let newClient = socket.remoteAddress;
-  socket.write("Hollaaaaa");
-  clientsList.push(newClient);
+  //the identity of the new client
+  let newClient = socket.remoteAddress + ":" + socket.remotePort;
+  //add the new client to the list
+  clientsList.push(socket);
+  //welcome the new client
 
-  const myFormat = printf(({ level, message, IP, label, timestamp }) => {
-    return `Time_Stamp: ${timestamp} | level: ${level}  | label:  ${label} | Message: ${message} | IP Address: ${IP}`;
-  });
-
-  const logger = createLogger({
-    //level: "info",
-    format: combine(
-      format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-      myFormat
-    ),
-    defaultMeta: { service: "user-service" },
-    transports: [new transports.File({ filename: "combined.txt" })],
-  });
+  //socket.write("Welcome " + newClient);
+  //notify to all the clients
+  broadcast(newClient + " joined", socket);
 
   logger.log({
     level: "info",
-    label: "New Client Connected!",
-    message: "Connected",
-    IP: server.address().address,
+    label: "client connected",
+    message: newClient + " connected",
+    IP: newClient,
   });
 
+  //handle new messages from clients
   socket.on("data", function (data) {
-    console.log("DATA " + socket.remoteAddress + ": " + data);
-    // Write the data back to all the connected, the client will receive it as data from the server
-    clientsList.forEach(function (sock, index, array) {
-      socket.write(
-        socket.remoteAddress + ":" + socket.remotePort + " said " + data + "\n"
-      );
-      console.log(
-        socket.remoteAddress +
-          ":" +
-          socket.remotePort +
-          " said " +
-          data.toString() +
-          "\n"
-      );
-    });
-  });
+    broadcast("\n" + newClient + ">>> " + data + "\n", socket);
 
-  socket.on("error", function (error) {
-    logger.log({
-      level: "error",
-      label: "Error occurred!",
-      message: error.message,
-      IP: server.address().address,
-    });
-    console.log("Error : " + error.stack);
-  });
+    if (data.toString() === "auto") {
+      logger.log({
+        level: "info",
+        label: "this level is information about the systme.",
+        message: data.toString() + " test is running",
+        IP: socket.remoteAddress + ":" + socket.remotePort,
+      });
 
-  socket.on("close", function (data) {
-    let index = clientsList.findIndex((client) => {
-      return client.remoteAddress === socket.remoteAddress;
-    });
-
-    if (index !== -1) {
-      clientsList.splice(index, 1);
-    }
-
-    clientsList.forEach((client) => {
-      socket.write(`${client.remoteAddress}: disconnected`);
-      console.log(`connection closed: ${client.remoteAddress}`);
+      logger.log({
+        level: "debug",
+        label: "this level is gives debug info about the system",
+        message: data.toString() + " test is running",
+        IP: socket.remoteAddress + ":" + socket.remotePort,
+      });
 
       logger.log({
         level: "warn",
-        label: "Client disconnected!",
-        message: data,
-        IP: socket.remoteAddress,
+        label: "this level is gives warninng about the system",
+        message: data.toString() + " test is running",
+        IP: socket.remoteAddress + ":" + socket.remotePort,
       });
-    });
+
+      logger.log({
+        level: "error",
+        label: "this level is gives errors about the system",
+        message: data.toString() + " test is running",
+        IP: socket.remoteAddress + ":" + socket.remotePort,
+      });
+    } else {
+      //console.log(data);
+      const payload = JSON.parse(data);
+      const logLevel = payload.level.toString();
+      const logDetails = payload.logMessage.toString();
+
+      if (logLevel === "info") {
+        logger.log({
+          level: "info",
+          label: "this level is information about the systme.",
+          message: logDetails,
+          IP: socket.remoteAddress + ":" + socket.remotePort,
+        });
+      }
+      if (logLevel === "debug") {
+        logger.log({
+          level: "debug",
+          label: "this level is gives debug info about the system",
+          message: logDetails,
+          IP: socket.remoteAddress + ":" + socket.remotePort,
+        });
+      }
+
+      if (logLevel === "warn") {
+        logger.log({
+          level: "warn",
+          label: "this level is gives warninng about the system",
+          message: logDetails,
+          IP: socket.remoteAddress + ":" + socket.remotePort,
+        });
+      }
+
+      if (logLevel === "error") {
+        logger.log({
+          level: "error",
+          label: "this level is gives errors about the system",
+          message: logDetails,
+          IP: socket.remoteAddress + ":" + socket.remotePort,
+        });
+      }
+    }
+  });
+
+  socket.on("error", function (error) {
+    console.log("Error : " + error.stack);
+  });
+
+  socket.on("end", function (data) {
+    clientsList.splice(clientsList.indexOf(socket), 1);
+    broadcast(newClient + " left the chat.\n");
+
+    console.log(newClient + " connection closed \n\n");
+
     logger.log({
-      level: "warn",
-      label: "Client disconnected!",
-      message: data,
-      IP: server.address().address,
+      level: "info",
+      label: "Client disconnected",
+      message: "client disconnected",
+      IP: socket.remoteAddress + ":" + socket.remotePort,
     });
-    console.log("connection closed \n\n");
   });
 });
+
+// Send a message to all clients
+function broadcast(message, sender) {
+  clientsList.forEach(function (client) {
+    // Don't want to send it to sender
+    if (client === sender) client.write("");
+    client.write(message);
+  });
+  // Log it to the server output too
+  console.log(message);
+}
+
+// clientsList.forEach((client) => {
+//   socket.write(`${client.remoteAddress}: disconnected`);
+//   console.log(`connection closed: ${client.remoteAddress}`);
+// });
+
+// const myFormat = printf(({ level, message, IP, label, timestamp }) => {
+//   return `Time_Stamp: ${timestamp} | level: ${level}  | label:  ${label} | Message: ${message} | IP Address: ${IP}`;
+// });
+
+// const logger = createLogger({
+//   //level: "info",
+//   format: combine(
+//     format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+//     myFormat
+//   ),
+//   defaultMeta: { service: "user-service" },
+//   transports: [new transports.File({ filename: "combined.txt" })],
+// });
+
+// logger.log({
+//   level: "info",
+//   label: "New Client Connected!",
+//   message: "Connected",
+//   IP: socket.remoteAddress + ":" + socket.remotePort,
+// });
